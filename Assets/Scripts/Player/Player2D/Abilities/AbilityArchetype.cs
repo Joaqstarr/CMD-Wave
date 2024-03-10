e85bb697a675611ecd9e1af1384641986e3a563c
halberdssb <jwstevenson22@gmail.com>
2024-03-09 20:23:36 -0500

    ability system and scan dart work (not finished yet)

--- Assets/Scripts/Player/Player2D/Abilities/AbilityArchetype.cs
+++ Assets/Scripts/Player/Player2D/Abilities/AbilityArchetype.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.WSA;
 
-public class AbilityArchetype : ScriptableObject
+public abstract class AbilityArchetype : ScriptableObject
 {
     public string commandShortcut;
+    public float cooldown;
 
-    public virtual void Activate(GameObject player)
+    public virtual void UseAbility(GameObject player, GameObject ability)
+    {
+
+    }
+
+    public virtual void UseAbility(GameObject player)
     {
 
     }
--- Assets/Scripts/Player/Player2D/Abilities/ScanDart.cs
+++ Assets/Scripts/Player/Player2D/Abilities/ScanDart.cs
@@ -1,18 +1,22 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScanDart : MonoBehaviour
+[CreateAssetMenu(menuName = "ScanDart/Scan Dart")]
+public class ScanDart : AbilityArchetype
 {
-    // Start is called before the first frame update
-    void Start()
+    public float launchForce;
+
+    public override void UseAbility(GameObject player, GameObject ability)
     {
-        
+        ability.transform.position = player.transform.position;
+        if (!ability.activeSelf) ability.SetActive(true);
+        ability.transform.parent = player.transform.Find("Abilities");
+        ability.GetComponent<CapsuleCollider>().enabled = true;
+
+        //ability.transform.rotation = new Vector3(ability.transform.rotation.x, ability.transform.rotation.y, SubViewCone.subAimAngle);
+        ability.GetComponent<Rigidbody>().AddForce(launchForce * SubViewCone.subAimVector);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
