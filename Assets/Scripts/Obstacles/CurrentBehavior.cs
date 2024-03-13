a85329ebff24af10e029d566d9f44c8fc600316b
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-03-12 22:20:34 -0400

    Currents
    TODO: Current Blockers
    Current scan

---
+++ Assets/Scripts/Obstacles/CurrentBehavior.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem), typeof(BoxCollider))]
+public class CurrentBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3[] _pushLocations;
+    [SerializeField]
+    private float _pushStrength = 5f;
+    [SerializeField]
+    Vector3 _particleDirection;
+
+    private ParticleSystem _particleSystem;
+    private BoxCollider _boxCollider;
+    private ParticleSystemForceField _particleSystemForceField;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        _boxCollider = GetComponent<BoxCollider>();
+       // _particleSystemForceField = GetComponent<ParticleSystemForceField>();
+
+        _particleDirection.Normalize();
+        //_particleSystemForceField.directionX = _particleDirection.x * _pushStrength;
+        //_particleSystemForceField.directionY = _particleDirection.y * _pushStrength;
+        //_particleSystemForceField.directionZ = _particleDirection.z * _pushStrength;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        for(int i = 0; i < _pushLocations.Length; i++)
+        {
+            Gizmos.DrawWireSphere(transform.TransformPoint(_pushLocations[i]), 0.5f);
+        }
+
+        Gizmos.color = Color.red;
+
+        Ray ray = new Ray(transform.position, _particleDirection);
+        Gizmos.DrawRay(ray);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb= other.GetComponent<Rigidbody>();
+        if(rb != null)
+        {
+            Vector3 positionToPushTowards = GetNearestPosition(other.transform.position);
+
+            Vector3 direction = positionToPushTowards - other.transform.position;
+
+            direction.Normalize();
+
+            rb.AddForce(direction * _pushStrength, ForceMode.Force);
+
+
+        }
+    }
+
+    private Vector3 GetNearestPosition(Vector3 position)
+    {
+        Vector3 closestPosition = transform.TransformPoint(_pushLocations[0]);
+        for(int i = 1; i < _pushLocations.Length; i++)
+        {
+            if(Vector3.Distance(position, transform.TransformPoint(_pushLocations[i])) < Vector3.Distance(position, closestPosition))
+                closestPosition = transform.TransformPoint(_pushLocations[i]);
+        }
+
+        return closestPosition;
+    }
+}
