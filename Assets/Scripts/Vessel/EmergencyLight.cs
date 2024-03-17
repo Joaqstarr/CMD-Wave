50f329ad5de9a37a4d40ac1b3ae74f2b548b2a35
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-03-17 13:36:03 -0400

    Alarm & Scan Dart sound

--- Assets/Scripts/Vessel/EmergencyLight.cs
+++ Assets/Scripts/Vessel/EmergencyLight.cs
@@ -8,11 +8,13 @@ public class EmergencyLight : MonoBehaviour
     private float _maxStrength;
     [SerializeField]
     private float _flashSpeed =5f;
+    private AudioSource _alarmSource;
     // Start is called before the first frame update
     void Start()
     {
         _emergenLight = GetComponent<Light>();
         _maxStrength = _emergenLight.intensity;
+        _alarmSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,15 @@ public class EmergencyLight : MonoBehaviour
         if (!EnemyDetector.EnemyNearby)
         {
             _emergenLight.intensity = 0;
-        }else
-        _emergenLight.intensity = ((Mathf.Sin(Time.time * _flashSpeed)/2)+0.5f) * _maxStrength;
+        }
+        else
+        {
+            float scalar = (Mathf.Sin(Time.time * _flashSpeed) / 2) + 0.5f;
+            _emergenLight.intensity = scalar * _maxStrength;
+            if(scalar > 0.5f && _alarmSource.isPlaying == false)
+            {
+                 _alarmSource.Play();
+            }
+        }
     }
 }
