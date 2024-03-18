9647fe73ac2f4f48339d525b6754f7c427bbd099
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2024-03-18 00:53:31 -0400

    Current scanner

---
+++ Assets/Scripts/Obstacles/CurrentPath.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentPath : MonoBehaviour
+{
+
+    public Collider Currentcollider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if(other.tag == "PlayerSub")
+        {
+            Physics.IgnoreCollision(Currentcollider, other, true);
+
+        }
+
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag == "PlayerSub")
+        {
+            Physics.IgnoreCollision(Currentcollider, other, true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "PlayerSub")
+        {
+            Physics.IgnoreCollision(Currentcollider, other, false);
+        }
+    }
+}
